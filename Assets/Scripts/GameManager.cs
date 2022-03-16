f1c1947ab09a3406f6618b482c366852e60f3201
krystianzun <krystian.zun@gmail.com>
2022-03-16 19:03:22 +0000

    Throw ball logic & animation

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,70 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float velocityFactor = 0.001f;
+    public float angleCorrection = 30;
+
+    public Rigidbody ballPrefab;
+    public Transform cameraTransform;
+    public AudioSource throwAudioSource;
+
+    private Vector2 startSwipePosition;
+    private float startSwipeTime;
+    private bool ballThrowingDisable;
+
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.touchCount != 1 || ballThrowingDisable)
+        {
+            return;
+        }
+
+        var touch = Input.GetTouch(0);
+
+        if (touch.phase == TouchPhase.Began)
+        {
+            startSwipePosition = touch.position;
+            startSwipeTime = Time.time;
+        }
+        else if(touch.phase == TouchPhase.Ended)
+        {
+            // get velocity of the swipe
+            var distance = (touch.position - startSwipePosition).magnitude;
+            var deltaTime = Time.time - startSwipeTime;
+            var velocity = distance / deltaTime;
+
+            if(velocity < 2500)
+            {
+                return;
+            }
+
+            ThrowBall(Mathf.Min(velocity, 10000));
+        }
+    }
+
+    private void ThrowBall(float velocity)
+    {
+        var position = cameraTransform.position + (cameraTransform.forward * 0.5f);
+        var ballRigidBody = Instantiate(ballPrefab, position, cameraTransform.rotation);
+
+        ballRigidBody.angularVelocity = Random.insideUnitSphere * Random.Range(0.5f, 2);
+
+        var direction = Vector3.RotateTowards(cameraTransform.forward, Vector3.up, Mathf.Deg2Rad * angleCorrection, 0);
+
+        ballRigidBody.velocity = direction * velocity * velocityFactor;
+
+        ballThrowingDisable = true;
+        throwAudioSource.Play();
+    }
+
+    public void BallDestroyed()
+    {
+        ballThrowingDisable = false;
     }
 }
